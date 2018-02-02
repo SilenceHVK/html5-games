9b4377e6670167927fd1c7e8648805d929a3e102
hvkcoder <hvkcoder@163.com>
2018-02-02 16:56:04 +0800

    Update Gobang

--- gobang/assets/js/app.js
+++ gobang/assets/js/app.js
@@ -2,6 +2,23 @@
     'use strict';
     const goBang = {};
     /**
+     * 添加矩阵属性
+     * @param {Number} m
+     * @param {Number} n
+     * @param {*} initial 
+     */
+    Array.prototype.matrix = function (m, n, initial) {
+        let mat = [];
+        for (let i = 0; i < m; i++) {
+            mat[i] = [];
+            for (let j = 0; j < n; j++) {
+                mat[i][j] = initial;
+            }
+        }
+        return mat;
+    };
+
+    /**
      * 初始化游戏界面
      */
     goBang.init = function () {
@@ -11,8 +28,14 @@
         document.body.appendChild(chess.canvas);;
         // 获取 Canvas 上下文对象
         chess.context = chess.canvas.getContext('2d');
+        // 声明玩家方
+        chess.player = true;
+        // 声明游戏是否结束
+        chess.gameOver = false;
         // 设置棋盘大小
         goBang.setChessBorderSize(chess);
+        // 绘制棋盘
+        goBang.drawChessBorder(chess);
     };
 
     /**
@@ -24,11 +47,41 @@
         let width = document.body.offsetHeight;
         let height = document.body.offsetWidth;
         // 取宽高整除 10
-        width = width - (width % 10);
-        height = height - (height % 10);
+        width = width - (width % 10) - 20;
+        height = height - (height % 10) - 20;
+        chess.chessBorderSize = width >= height ? height : width;
         // 宽高比较取最小值
-        chess.canvas.width = chess.canvas.height = width >= height ? height : width;
+        chess.canvas.width = chess.canvas.height = chess.chessBorderSize;
     };
+
+    /**
+     * 绘制棋盘
+     * @param {Object} chess 
+     * @param {Number} interval 
+     * @param {Number} padding 
+     */
+    goBang.drawChessBorder = function (chess, interval = 30, padding = 10) {
+        if (chess) {
+            const spliteNum = Math.round(chess.chessBorderSize / interval);
+            chess.context.strokeStyle = '#bfbfbf';
+            for (let i = 0; i < spliteNum; i++) {
+                chess.context.beginPath();
+                chess.context.moveTo(i * interval + padding, padding);
+                chess.context.lineTo(i * interval + padding, chess.canvas.height - padding);
+                chess.context.stroke();
+                chess.context.closePath();
+
+                chess.context.beginPath();
+                chess.context.moveTo(padding, i * interval + 10);
+                chess.context.lineTo(chess.canvas.width - 10, i * interval + 10);
+                chess.context.stroke();
+                chess.context.closePath();
+            }
+
+
+        }
+    };
+
     window.goBang = goBang;
 })();
 
