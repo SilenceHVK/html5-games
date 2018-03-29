4e8a8f256bb01c405beaa32be7942882c062b023
hvkcoder <hvkcoder@163.com>
2018-03-29 23:21:00 +0800

    Update app.js

--- tetris/assets/js/app.js
+++ tetris/assets/js/app.js
@@ -6,8 +6,28 @@
     };
 
     DomObject.prototype.get = function () {
-        return this.dom();
+        return this.dom;
     };
 
+    DomObject.prototype.on = function (eventName, eventHandler) { // 事件注册
+        this.get().addEventListener(eventName, eventHandler, false);
+    };
 
+    DomObject.prototype.css = function (styleKey, styleValue) {
+        this.get().style[styleKey] = styleValue;
+    };
+
+    function $(selector, context) {    //context在哪里去找，如果没有就用document
+        return new DomObject((context || document).querySelector(selector));
+    }
+
+    function _init() {
+        $('#btn-start').on('click', function () {
+            alert('开始游戏');
+        });
+    };
+
+    document.addEventListener('DOMContentLoaded', function (ev) {
+        _init();
+    })
 })(document);
